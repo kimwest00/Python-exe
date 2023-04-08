45a1e90172117a2971c8e584dd3ad74585a72cd3
kimwest00 <minseohufs@hufs.ac.kr>
2023-04-09 04:55:36 +0900

    feat - detail 출력되도록 설정

--- opendata-crawler/notice-api.py
+++ opendata-crawler/notice-api.py
@@ -42,14 +42,24 @@ data_list = element.find_elements(By.TAG_NAME , 'li')
 for data in data_list:
     detail_title_area = data.find_element(By.CSS_SELECTOR ,'dl')
     title = detail_title_area.find_element(By.CSS_SELECTOR ,'dt a span.title').text
+
+    # display 속성을 변경하여 화면에 표시되도록 만듦
+    # @media 태그로 인해서 바로 수정되지않아, viewport 크기를 변경함으로써
+    # 수정될수있도록 함
+    driver.execute_script(
+        "Object.defineProperty(window, 'innerWidth', {value: 1025, configurable: true});var dd_list = document.querySelectorAll('.data-set-list .result-list ul li dl dd');for (var i = 0; i < dd_list.length; i++) {dd_list[i].style.display = 'block';}")
+    details = detail_title_area.find_elements(By.TAG_NAME ,"dd")
     link = detail_title_area.find_element(By.CSS_SELECTOR ,'dt a').get_attribute('href')
+    for detail in details:
+        print(detail.text)
+    
     info_data = data.find_elements(By.CSS_SELECTOR ,'div.info-data p')
     for info in info_data:
         print(info.find_element(By.CSS_SELECTOR ,'span.tit').text)
         try:
             print(info.find_element(By.CSS_SELECTOR ,'span.data').text)
         except:
-            print(info.text.split(' ')[1].split(','))
+             print(info.text.split(' ')[1].split(','))
 
 # iframe 바깥으로 이동
 driver.switch_to.default_content()
--- opendata-crawler/tempCodeRunnerFile.py
+++ opendata-crawler/tempCodeRunnerFile.py
@@ -1,10 +1,1 @@
-# iframe 내부로 이동
-driver.switch_to.frame(iframe)
-# #document 요소 선택
-document = driver.find_element(By.ID ,'#document')
-
-# #document 값 출력
-print(document.text)
-
-# iframe 바깥으로 이동
-driver.switch_to.default_content()
+    detail = detail_title_area.find_elements(By.TAG_NAME ,"dd")
