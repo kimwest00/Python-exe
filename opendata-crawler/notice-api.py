02da620e5cec70b60a8b402f013c9a6b8a9cc0b0
kimwest00 <minseohufs@hufs.ac.kr>
2023-04-09 03:58:53 +0900

    feat - 요구하는 데이터 변수화및 출력

--- opendata-crawler/notice-api.py
+++ opendata-crawler/notice-api.py
@@ -1,7 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from openpyxl import Workbook
 
+# 가져온 데이터를 기반으로 엑셀 파일 생성
+def writeWorkbook():
+    write_wb = Workbook()
+    write_ws = write_wb.create_sheet('참석자 시트')
+    write_ws = write_wb.active
+    for i in range(2,15):
+        for j in i_list:
+            people = load_ws.cell(i,j).value
+            if people:
+                people = removeSlash(people)
+                write_ws.append(people)
+    write_wb.save("excelConvert/testOutput.xlsx")   
+    
 # ChromeDriver 경로 설정
 DRIVER_PATH = 'C:/Users/MinSeo/Downloads/chromedriver_win32/chromdriver.exe'
 URL = 'https://www.moel.go.kr/info/publicdata/publicopen/list.do'
@@ -26,7 +40,16 @@ element = driver.find_element(By.CSS_SELECTOR ,'#fileDataList div.result-list ul
 #list로 가져오기
 data_list = element.find_elements(By.TAG_NAME , 'li')
 for data in data_list:
-    print(data.find_element(By.CSS_SELECTOR ,'span.data').text)
+    detail_title_area = data.find_element(By.CSS_SELECTOR ,'dl')
+    title = detail_title_area.find_element(By.CSS_SELECTOR ,'dt a span.title').text
+    link = detail_title_area.find_element(By.CSS_SELECTOR ,'dt a').get_attribute('href')
+    info_data = data.find_elements(By.CSS_SELECTOR ,'div.info-data p')
+    for info in info_data:
+        print(info.find_element(By.CSS_SELECTOR ,'span.tit').text)
+        try:
+            print(info.find_element(By.CSS_SELECTOR ,'span.data').text)
+        except:
+            print(info.text.split(' ')[1].split(','))
 
 # iframe 바깥으로 이동
 driver.switch_to.default_content()
