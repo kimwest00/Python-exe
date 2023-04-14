438359e5870dbc9411d28b40d1ccbf810b689bda
kimwest00 <minseohufs@hufs.ac.kr>
2023-04-15 01:56:07 +0900

    feat - pagination 적용

--- opendata-crawler/notice-api.py
+++ opendata-crawler/notice-api.py
@@ -3,18 +3,15 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from openpyxl import Workbook
 
+#TODO:excel에 데이터 저장
 # 가져온 데이터를 기반으로 엑셀 파일 생성
-def writeWorkbook():
+def writeWorkbook(i,j):
     write_wb = Workbook()
     write_ws = write_wb.create_sheet('참석자 시트')
     write_ws = write_wb.active
-    for i in range(2,15):
-        for j in i_list:
-            people = load_ws.cell(i,j).value
-            if people:
-                people = removeSlash(people)
-                write_ws.append(people)
-    write_wb.save("excelConvert/testOutput.xlsx")   
+    # for j in i_list:
+    #     write_ws.append(people)
+    write_wb.save("./testOutput.xlsx")   
     
 # ChromeDriver 경로 설정
 DRIVER_PATH = 'C:/Users/MinSeo/Downloads/chromedriver_win32/chromdriver.exe'
@@ -35,34 +32,42 @@ driver.get(URL)
 iframe = driver.find_element(By.NAME,'frm')
 # iframe 내부로 이동
 driver.switch_to.frame(iframe)
+
+# pagination 적용
+
+
 # 정보 요소 가져오기
-element = driver.find_element(By.CSS_SELECTOR ,'#fileDataList div.result-list ul')
-#list로 가져오기
-data_list = element.find_elements(By.TAG_NAME , 'li')
-for data in data_list:
-    detail_title_area = data.find_element(By.CSS_SELECTOR ,'dl')
-    title = detail_title_area.find_element(By.CSS_SELECTOR ,'dt a span.title').text
+for page in range(1,19):
+    element = driver.find_element(By.CSS_SELECTOR ,'#fileDataList div.result-list ul')
+    #list로 가져오기
+    data_list = element.find_elements(By.TAG_NAME , 'li')
+    for data in data_list:
+        detail_title_area = data.find_element(By.CSS_SELECTOR ,'dl')
+        title = detail_title_area.find_element(By.CSS_SELECTOR ,'dt a span.title').text
 
-    # display 속성을 변경하여 화면에 표시되도록 만듦
-    # @media 태그로 인해서 바로 수정되지않아, viewport 크기를 변경함으로써
-    # 수정될수있도록 함
-    driver.execute_script(
-        "Object.defineProperty(window, 'innerWidth', {value: 1025, configurable: true});var dd_list = document.querySelectorAll('.data-set-list .result-list ul li dl dd');for (var i = 0; i < dd_list.length; i++) {dd_list[i].style.display = 'block';}")
-    details = detail_title_area.find_elements(By.TAG_NAME ,"dd")
-    link = detail_title_area.find_element(By.CSS_SELECTOR ,'dt a').get_attribute('href')
-    for detail in details:
-        print(detail.text)
-    
-    info_data = data.find_elements(By.CSS_SELECTOR ,'div.info-data p')
-    for info in info_data:
-        print(info.find_element(By.CSS_SELECTOR ,'span.tit').text)
-        try:
-            print(info.find_element(By.CSS_SELECTOR ,'span.data').text)
-        except:
-             print(info.text.split(' ')[1].split(','))
-
+        # display 속성을 변경하여 화면에 표시되도록 만듦
+        # @media 태그로 인해서 바로 수정되지않아, viewport 크기를 변경함으로써
+        # 수정될수있도록 함
+        driver.execute_script(
+            "Object.defineProperty(window, 'innerWidth', {value: 1025, configurable: true});var dd_list = document.querySelectorAll('.data-set-list .result-list ul li dl dd');for (var i = 0; i < dd_list.length; i++) {dd_list[i].style.display = 'block';}")
+        details = detail_title_area.find_elements(By.TAG_NAME ,"dd")
+        link = detail_title_area.find_element(By.CSS_SELECTOR ,'dt a').get_attribute('href')
+        for detail in details:
+            print(detail.text)
+        
+        info_data = data.find_elements(By.CSS_SELECTOR ,'div.info-data p')
+        for info in info_data:
+            print(info.find_element(By.CSS_SELECTOR ,'span.tit').text)
+            try:
+                print(info.find_element(By.CSS_SELECTOR ,'span.data').text)
+            except:
+                print(info.text.split(' ')[1].split(','))
+        driver.execute_script(
+            "$('#search-form-current-page').val(arguments[0]);",page
+        )
 # iframe 바깥으로 이동
 driver.switch_to.default_content()
 
 # 웹 브라우저 종료
 driver.quit()
+
