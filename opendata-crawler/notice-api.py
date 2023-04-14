f032c29ddbdf2f3052eebd9cfe9e85b3f489f97e
kimwest00 <minseohufs@hufs.ac.kr>
2023-04-15 02:52:18 +0900

    fix - index 설정 오류 수정

--- opendata-crawler/notice-api.py
+++ opendata-crawler/notice-api.py
@@ -23,7 +23,7 @@ write_wb = Workbook()
 sheet = write_wb.active
 sheet.title = "고용노동 데이터"
 col_name = [e.value for e in ColNameKey]
-print(col_name)
+# print(col_name)
 for idx, name in enumerate(col_name):
     sheet.cell(row=1,column=idx+1,value=name)
 def writeWorkbook(idx,key,value):
@@ -54,16 +54,19 @@ driver.switch_to.frame(iframe)
 
 # pagination 적용
 
-
+index = 2
 # 정보 요소 가져오기
 for page in range(1,19):
+    driver.execute_script(
+        "$('#search-form-current-page').val(arguments[0]);",page
+    )
     element = driver.find_element(By.CSS_SELECTOR ,'#fileDataList div.result-list ul')
     #list로 가져오기
     data_list = element.find_elements(By.TAG_NAME , 'li')
     for data in data_list:
         detail_title_area = data.find_element(By.CSS_SELECTOR ,'dl')
         title = detail_title_area.find_element(By.CSS_SELECTOR ,'dt a span.title').text
-        writeWorkbook(page,ColNameKey.TITLE,title)
+        writeWorkbook(index,ColNameKey.TITLE,title)
         # display 속성을 변경하여 화면에 표시되도록 만듦
         # @media 태그로 인해서 바로 수정되지않아, viewport 크기를 변경함으로써
         # 수정될수있도록 함
@@ -71,12 +74,12 @@ for page in range(1,19):
             "Object.defineProperty(window, 'innerWidth', {value: 1025, configurable: true});var dd_list = document.querySelectorAll('.data-set-list .result-list ul li dl dd');for (var i = 0; i < dd_list.length; i++) {dd_list[i].style.display = 'block';}")
         details = detail_title_area.find_elements(By.TAG_NAME ,"dd")
         link = detail_title_area.find_element(By.CSS_SELECTOR ,'dt a').get_attribute('href')
-        writeWorkbook(page,ColNameKey.LINK,link)
+        writeWorkbook(index,ColNameKey.LINK,link)
 
         for detail in details:
             detail_list = ''
             detail_list += detail.text
-            writeWorkbook(page,ColNameKey.DETAIL,detail_list)
+            writeWorkbook(index,ColNameKey.DETAIL,detail_list)
         info_data = data.find_elements(By.CSS_SELECTOR ,'div.info-data p')
         for info in info_data:
             span_title = info.find_element(By.CSS_SELECTOR ,'span.tit').text
@@ -85,11 +88,9 @@ for page in range(1,19):
             except:
                 tmp_span_data = info.text.split(' ')[1].split(',')
                 span_data = ','.join(map(str, tmp_span_data))
-            
-            writeWorkbook(page,ColNameKey(span_title).value,span_data)
-        driver.execute_script(
-            "$('#search-form-current-page').val(arguments[0]);",page
-        )
+            writeWorkbook(index,ColNameKey(span_title).value,span_data)
+        index += 1
+    
         
 
 # iframe 바깥으로 이동
